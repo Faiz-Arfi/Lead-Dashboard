4799c1a85533d8c4b98cba755970c275b14d35ac
Faiz Arfi <faizarfi12@gmail.com>
2026-01-17 20:51:56 +0530

    fixed the cors error

--- lead-bashboard-frontend/vite.config.js
+++ lead-bashboard-frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // 1. Capture any request starting with "/api"
+      '/api': {
+        // 2. Forward it to your Render Backend
+        target: 'https:/api-leadcrm.faizarfi.dev/',
+        
+        // 3. IMPORTANT: This tells the server "I am coming from the cloud", 
+        // effectively lying about the origin to bypass security checks.
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
